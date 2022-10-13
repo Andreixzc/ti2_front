45398be4413bc9db07df2dbd66902142e7aace30
marco <marcoxd01@hotmail.com>
2022-10-12 21:23:18 -0300

    fix html path

--- src/scripts/cadastro.js
+++ src/scripts/cadastro.js
@@ -4,12 +4,16 @@ formCadastrar.addEventListener("submit", validar);
 function validar(event) {
   event.preventDefault();
   const formDados = new FormData(event.target);
+  let empresario = false;
   // console.log(Object.fromEntries(formDados),'saj0ijiasn')
   const novoUsuario = JSON.stringify(Object.fromEntries(formDados));
+  if (JSON.parse(novoUsuario).vendedor == "SIM") {
+    empresario = true;
+  }
   formCadastrar.reset();
-  registerUser(novoUsuario);
+  registerUser(novoUsuario, empresario);
 }
-async function registerUser(data) {
+async function registerUser(data, empresario) {
   await fetch(urlInsertUser, {
     method: "POST",
     body: data,
@@ -17,6 +21,13 @@ async function registerUser(data) {
     .then((res) => res.json())
     .then((res) => res)
     .catch((err) => window.alert(err.message));
+
+  if (empresario) {
+    setTimeout(() => {
+      window.location = "/src/pages/Empresa.html";
+    }, 500);
+    return;
+  }
 
   setTimeout(() => {
     window.location = "/login.html";
