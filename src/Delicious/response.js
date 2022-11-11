58b25f66bb10cd1011594da2fdc860a08244fc13
Andreixzc <andrei.massaini@hotmail.com>
2022-11-11 15:17:24 -0300

    si

--- src/Delicious/response.js
+++ src/Delicious/response.js
@@ -1,18 +1,21 @@
-const urlPerfil = "https://expresso-fiesta.herokuapp.com/local/1"
+const urlLocal = "https://expresso-fiesta.herokuapp.com/local/1"
+const urlAtracao = "https://expresso-fiesta.herokuapp.com/atracao/1"
+const urlAlimento = "https://expresso-fiesta.herokuapp.com/alimento/2"
+
 const responseObj = {
-    local : "uhehue"
+    local : "",
+    atracao:"",
+    alimento:""
   };
   
   carregaDados();
   
 
   async function carregaDados() {
-    const data = await fetch(urlPerfil).then((res )=> res.json()).then((res)=>res).catch((error)=>console.log(error))
-    console.log(data.nome)
-    responseObj.local = data.nome;
-    return data.nome;
+    const data1 = await fetch(urlLocal).then((res )=> res.json()).then((res)=>res).catch((error)=>console.log(error))
+    const data2 = await fetch(urlAtracao).then((res )=> res.json()).then((res)=>res).catch((error)=>console.log(error))
+    const data3 = await fetch(urlAlimento).then((res )=> res.json()).then((res)=>res).catch((error)=>console.log(error))
+    responseObj.local = data1.nome;
+    responseObj.atracao = data2.nome;
+    responseObj.alimento = data3.nome;
   }
-      
-  
-      
-  
